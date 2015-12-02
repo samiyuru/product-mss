f0c043d670232517c3c0055847a6149cbfc7388a
Samiyuru <samiyuru@wso2.com>
2015-12-02 09:06:20 +0530

    HttpMethodInfo implementation for lambda handlers

---
+++ carbon-mss/components/org.wso2.carbon.mss/src/main/java/org/wso2/carbon/mss/internal/router/LambdaMethodInfo.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright (c) WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.mss.internal.router;
+
+import io.netty.handler.codec.http.HttpContent;
+import io.netty.handler.codec.http.HttpRequest;
+import org.wso2.carbon.mss.HttpResponder;
+import org.wso2.carbon.mss.Route;
+import org.wso2.carbon.mss.internal.router.api.HttpMethodInfo;
+
+import java.util.Map;
+
+public class LambdaMethodInfo implements HttpMethodInfo {
+
+    private HttpRequest httpRequest;
+    private HttpResponder httpResponder;
+    private Map<String, String> groupValues;
+    private Route route;
+
+    public LambdaMethodInfo(HttpRequest httpRequest,
+                            HttpResponder httpResponder,
+                            Map<String, String> groupValues,
+                            Route route) {
+        this.httpRequest = httpRequest;
+        this.httpResponder = httpResponder;
+        this.groupValues = groupValues;
+        this.route = route;
+    }
+
+    @Override
+    public void invoke() {
+
+    }
+
+    @Override
+    public void chunk(HttpContent chunk) {
+
+    }
+}
